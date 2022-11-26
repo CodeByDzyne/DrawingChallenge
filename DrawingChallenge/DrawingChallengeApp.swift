d01628e3420540cd9a44f58b94e029619d54e4c1
CodeByDzyne <wdenley@dzynesolutions.com>
2022-11-26 12:15:20 +1100

    Initial Commit

---
+++ DrawingChallenge/DrawingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingChallengeApp.swift
+//  DrawingChallenge
+//
+//  Created by Wealthy Waz on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
